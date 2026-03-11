70ece13c2b208b609174381e774b49a734fefde7
raghav <guptaraghav284@gmail.com>
2026-03-11 18:57:50 +0530

    feat: add auth pages and update UI components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
-import Landing from './pages/Landing'
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Auth from "./pages/Auth";
 
 function App() {
-  return <Landing />
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
